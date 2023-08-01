5390fe269eba52022486b28cd200ac766719fe77
jayanth-parthsarathy <jparthaa@gmail.com>
2023-08-01 11:13:23 +0530

    Setup socket io on client and server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import JoinRoom from "./pages/JoinRoom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Room from "./pages/Room";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:3000");
 export default function App() {
   const router = createBrowserRouter([
     {
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,19 +6,30 @@ import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import roomRoutes from "./routes/roomRoutes";
 import messageRoutes from "./routes/messageRoutes";
+import http from "http";
+import { Server } from "socket.io";
 dotenv.config();
 const port = process.env.PORT || 5000;
 connectToDB();
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/room", roomRoutes);
-
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+server.listen(port, () => {
   console.log("Server running on port " + port);
 });
